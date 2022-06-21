81f67cf8f8231de85b5c10f81bad4e1b92338210
Aniket Pandey <77108290+drstrange001@users.noreply.github.com>
2022-06-21 23:17:25 +0530

    Time: 4 ms (31.96%), Space: 48 MB (40.08%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -8,21 +8,33 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Hey, This might not be the best approach
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        //check for empty list
         if(l1 == null || l2 == null) return l1 != null ? l1 : l2;
         
-        ListNode head= new ListNode(-1);
-        ListNode itr= head;
+        ListNode head = new ListNode(-1);
+        ListNode itr = head;
         int carry=0;
+        
         while(l1!=null || l2!=null || carry!=0){
-            int sum = carry + (l1 != null ? l1.val : 0) +(l2 != null ? l2.val : 0);
+            int sum = carry + (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0);
+            
+            //get value for list
             int digit = sum%10;
+           
+            //get carry
             carry = sum/10;
-            itr.next=new ListNode(digit);
-            itr=itr.next;
-            if(l1 != null) l1=l1.next;
-            if(l2 != null) l2=l2.next;
+            
+            //new node
+            itr.next = new ListNode(digit);
+            
+            itr = itr.next;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
         }
         return head.next;
     }
