9a74a800b81e739d0529609a0bcd4a11d11dda93
Aniket Pandey <77108290+drstrange001@users.noreply.github.com>
2022-06-25 14:06:27 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (89.52%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public int fib(int n) {
-        int dp[]= new int[n+1];
-        return fibDp(n,dp);
-    }
-    
-    public int fibDp(int n, int[] dp){
         if(n<=1) return n;
-        if(dp[n]!=0) return dp[n];
+        int dp[] = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
         
-        dp[n]=fibDp(n-1,dp)+fibDp(n-2,dp);
+        for(int i=2; i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
+    
 }
