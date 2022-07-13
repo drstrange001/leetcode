dd56954441c1334c19c96b0eba4e742b00e7f53a
Aniket Pandey <77108290+drstrange001@users.noreply.github.com>
2022-07-13 23:20:45 +0530

    Time: 5 ms (70.27%), Space: 45.1 MB (5.23%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,42 +1,39 @@
 class Solution {
-    public static int[]  nextGreaterElementInArray(int[] nums2){
-        int[] nge =new int[nums2.length];
+    
+    public static int[] nextg(int[] nums2){
         Stack<Integer> st= new Stack<>();
-        
+        int[] nge= new int[nums2.length];
         st.push(0);
         
-        for(int i=0;i<nums2.length;i++){
+        for (int i=0; i<nums2.length;i++){
             while(st.size()>0 && nums2[i]>nums2[st.peek()]){
-                int pos= st.peek();
+                int pos=st.peek();
                 nge[pos]=nums2[i];
-                st.pop(); 
+                st.pop();
             }
             st.push(i);
         }
         
-        while(st.size()!=0){
+        while(st.size()>0){
             int pos= st.peek();
             nge[pos]=-1;
-            st.pop(); 
+            st.pop();
         }
         return nge;
     }
     
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] nxge= nextGreaterElementInArray(nums2);
+        int[] nxg=nextg(nums2);
+        int[] res= new int[nums1.length];
+        HashMap<Integer, Integer> map= new HashMap<>();
         
-        int[] res=new int[nums1.length];
-        HashMap<Integer,Integer> map=new HashMap<>();
-        
-        for(int i=0;i<nums2.length;i++){
-            map.put(nums2[i],nxge[i]);
+        for(int i=0; i<nums2.length;i++){
+            map.put(nums2[i],nxg[i]);
         }
         
         for(int i=0; i<nums1.length;i++){
             res[i]=map.get(nums1[i]);
         }
-        
         return res;
-        
     }
 }
