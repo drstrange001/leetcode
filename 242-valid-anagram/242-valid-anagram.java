39821f045b18a6cf4c1ff1aac362d1ede4962024
Aniket Pandey <77108290+drstrange001@users.noreply.github.com>
2022-07-19 13:41:35 +0530

    Time: 14 ms (35.51%), Space: 43.1 MB (82.94%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        
+        HashMap<Character, Integer> map= new HashMap<>();
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(map.containsKey(ch)){
+            int cf=map.get(ch);
+            int nf=cf+1;
+            map.put(ch,nf);
+            }
+            else {
+                map.put(ch,1);
+            }
+        }
+        
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(map.containsKey(ch) && map.get(ch)!=0){
+                int cf=map.get(ch);
+                int nf=cf-1;
+                map.put(ch,nf);
+            } else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
