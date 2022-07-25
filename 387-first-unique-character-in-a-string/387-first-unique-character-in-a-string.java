3a6f57d4cad78a62b0d8e57bbe35e1a7c3b97de2
Aniket Pandey <77108290+drstrange001@users.noreply.github.com>
2022-07-25 10:56:23 +0530

    Time: 28 ms (63.85%), Space: 42.9 MB (88.53%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -4,25 +4,19 @@ class Solution {
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(freq.containsKey(ch)){
-                int cf=freq.get(ch);
-                int nf=cf+1;
-                freq.put(ch,nf);
+                freq.put(ch,2);
             } else {
                 freq.put(ch,1);
             }
         }
-        int c=0;
+
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             if(freq.containsKey(ch)&&(freq.get(ch)==1)){
-                c=i;
-                return c;
-            } else{
-                c++;
-            }
+                return i;
+            } 
         }
-        
-        if(c>s.length()-1) c=-1;
-        return c;
+
+        return -1;
     }
 }
